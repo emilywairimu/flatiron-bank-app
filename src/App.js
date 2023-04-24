5a58a1861653f0fab80acef10bf63705784f63d6
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-04-24 03:17:25 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 
-// import Button from "./components/Button";
 import Form from "./components/Form";
 import Header from "./components/Header";
-// import SearchBar from "./components/SearchBar";
 import Transaction from "./components/Transaction";
 
 function App() {
   return (
     <div>
       <Header />
-      {/* <SearchBar /> */}
       <Form />
-      {/* <Button /> */}
       <Transaction />
     </div>
   );
